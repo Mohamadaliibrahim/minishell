8b2386858782e72a60a22ba58fe64ac132876475
Mohammad Ali Ibrahim <mohamibr@c1r4s2.42beirut.com>
2024-09-12 17:53:30 +0300

    Upload

--- Minishell/src/ft_echo.c
+++ Minishell/src/ft_echo.c
@@ -10,6 +10,23 @@ void	skip_echo(char **input)
 		while (**input == ' ')
 			(*input)++;
 	}
+
+}
+
+int	handle_echo_n(char **input)
+{
+	int	has_n_flag;
+
+	has_n_flag = 0;
+	while (ft_strncmp(*input, "-n", 2) == 0
+		&& (*input)[2] == ' ')
+	{
+		has_n_flag = 1;
+		*input += 2;
+		while (**input == ' ')
+			(*input)++;
+	}
+	return (has_n_flag);
 }
 
 char	*extract_quoted_token(char **input, char quote_type)
@@ -37,10 +54,14 @@ void check_echo(char *input, char *start, char	*quoted_token)
 	t_echo	*new_node;
 	t_echo	*next;
 	t_echo	*input_tokens;
+	int		has_n_flag;
 
 	current = NULL;
 	input_tokens = NULL;
 	skip_echo(&input);
+	has_n_flag = handle_echo_n(&input);
+	if (*input == '\0')
+		return ;
 	while (*input)
 	{
 		while (*input == ' ')
@@ -86,10 +107,11 @@ void check_echo(char *input, char *start, char	*quoted_token)
 	{
 		while (current)
 		{
-			printf("%s ", current->token);
+			printf("%s", current->token);
 			current = current->next;
 		}
-		printf("\n");
+		if (!has_n_flag)
+			printf("\n");
 	}
 	current = input_tokens;
 	while (current)
