aedfa709fdb532e0a29ea9bbfaf65604e0a76992
Mustafa Machlouch <machlouchcr777@gmail.com>
2024-09-19 12:56:21 +0300

    SIGNALS

--- Minishell/src/parsing/signals.c
+++ Minishell/src/parsing/signals.c
@@ -6,7 +6,7 @@
 /*   By: mustafa-machlouch <mustafa-machlouch@st    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 11:24:57 by mustafa-mac       #+#    #+#             */
-/*   Updated: 2024/09/19 11:54:33 by mustafa-mac      ###   ########.fr       */
+/*   Updated: 2024/09/19 12:52:51 by mustafa-mac      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,13 +18,14 @@ static void handle_sigint(int sig)
     write(1, "\n\033[0;36m$>\033[0m ", 15);
 }
 
-
-static void    handle_sigquit(int sig)
+static void handle_sigquit(int sig)
 {
     (void)sig;
+    // You can ignore SIGQUIT entirely if you don't want it to do anything:
+    signal(SIGQUIT, SIG_IGN);
 }
 
-void    setup_signal_handlers(void)
+void setup_signal_handlers(void)
 {
     signal(SIGINT, handle_sigint);
     signal(SIGQUIT, handle_sigquit);
