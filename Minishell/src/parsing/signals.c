6658fd04ead0833c6169802dd814ab721f924da4
Mustafa Machlouch <machlouchcr777@gmail.com>
2024-09-19 11:55:12 +0300

    SIGNALS

--- Minishell/src/parsing/signals.c
+++ Minishell/src/parsing/signals.c
@@ -6,20 +6,20 @@
 /*   By: mustafa-machlouch <mustafa-machlouch@st    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/19 11:24:57 by mustafa-mac       #+#    #+#             */
-/*   Updated: 2024/09/19 11:43:27 by mustafa-mac      ###   ########.fr       */
+/*   Updated: 2024/09/19 11:54:33 by mustafa-mac      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../inc/minishell.h"
 
-void handle_sigint(int sig)
+static void handle_sigint(int sig)
 {
     (void)sig;
     write(1, "\n\033[0;36m$>\033[0m ", 15);
 }
 
 
-void    handle_sigquit(int sig)
+static void    handle_sigquit(int sig)
 {
     (void)sig;
 }
