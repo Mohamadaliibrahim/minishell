f21e872037fd44f88dd35e556560e694e6de5bfa
Mohammad Ali Ibrahim <mohamibr@c1r5s7.42beirut.com>
2024-10-04 13:56:10 +0300

    fix stuff in redirections and heredoc and the exitstatus after exit

--- .push.sh
+++ .push.sh
@@ -1,1 +1,1 @@
-git add . && git commit -m "fix leaks and errors and edge cases in the redirection and heredoc" && git status && git push -f
+git add . && git commit -m "fix stuff in redirections and heredoc and the exitstatus after exit " && git status && git push -f
